d9483381455921e9c2fb0be2a04985dbd6b3cb16
Dev <lodhad@gmail.com>
2016-12-18 02:15:57 -0700

    Added missing flags

--- io/HttpFlags.cpp
+++ io/HttpFlags.cpp
@@ -6,6 +6,12 @@ uint32_t io::flag::PUT = 0x00000004;
 uint32_t io::flag::DELETE = 0x00000008;
 uint32_t io::flag::OPTIONS = 0x00000010;
 uint32_t io::flag::CONNECT = 0x00000020;
+uint32_t io::flag::KEEPALIVE = 0x00000040;
 
-uint32_t io::flag::WEBSOCKET = 0x00000040;
-uint32_t io::flag::KEEPALIVE = 0x00000080;
+uint32_t io::flag::WEBSOCKET = 0x00000080;
+uint32_t io::flag::KEEPALIVE = 0x00000100;
+
+uint32_t io::flag::COOKIE_EXPIRES = 0x00000200;
+uint32_t io::flag::COOKIE_SECURE = 0x00000400;
+uint32_t io::flag::COOKIE_HTTPONLY = 0x00000800;
+uint32_t io::flag::COOKIE_SAMESITE = 0x00001000;
